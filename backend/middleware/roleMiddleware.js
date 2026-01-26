5368cfddb55d7be31cdaef2c668a6258ea680b5b
Vivekvadher1 <101575019+Vivekvadher1@users.noreply.github.com>
2026-01-26 12:39:13 +0530

    Update roleMiddleware.js

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,8 +1,8 @@
-export const authorizeRoles = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-    next();
-  };
-};
+// export const authorizeRoles = (...roles) => {
+//   return (req, res, next) => {
+//     if (!roles.includes(req.user.role)) {
+//       return res.status(403).json({ message: "Access denied" });
+//     }
+//     next();
+//   };
+// };
